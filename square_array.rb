3cd5d916a26ddb47782376150b1f8c2c5fcb223e
Jharell Chambers <jharell.chambers@selu.edu>
2019-08-13 17:41:43 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,17 @@
 def square_array(array)
-  # your code here
-end
+ arr_sqr = []
+ 
+ i = 0 
+  array.each do |element| 
+   old_arr = array[i]
+   new_arr = old_arr ** 2
+   arr_sqr << new_arr 
+ 
+ i += 1 
+ end
+ 
+ return arr_sqr
+ end
+ 
+ 
+ p square_array([1,2,3,])
